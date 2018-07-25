5d2e5ba5f53edb9c3c433fce6631e3133e8f08bd
Hristo Dimitrov <hristodimitrov9119@gmail.com>
2018-07-25 21:33:19 +0300

    Fix app name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/bioinformatics'));
+app.use(express.static(__dirname + '/dist/bioinformatics-fe'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/bioinformatics/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bioinformatics-fe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
