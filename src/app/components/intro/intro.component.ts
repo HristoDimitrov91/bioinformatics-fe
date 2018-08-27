80d365a81d4342d7889c6d5d5fc8b81a8e4fbf6b
Hristo Dimitrov <hristo.dimitrov@paysafe.com>
2018-08-27 17:11:15 +0300

    Add animation for bioinformatics circle

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -9,14 +9,14 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
     trigger('scrollAnimation', [
       state('show', style({
         opacity: 1,
-        transform: "translateX(0)"
+        transform: "translateX(35%)"
       })),
       state('hide', style({
         opacity: 0,
         transform: "translateX(-100%)"
       })),
-      transition('show => hide', animate('700ms ease-out')),
-      transition('hide => show', animate('700ms ease-in'))
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('300ms ease-in'))
     ])
   ]
 })
@@ -31,7 +31,7 @@ export class IntroComponent {
     const componentPosition = this.el.nativeElement.offsetTop
     const scrollPosition = window.pageYOffset
 
-    if (scrollPosition >= componentPosition) {
+    if (scrollPosition > componentPosition + 100) {
       this.state = 'show'
     } else {
       this.state = 'hide'
